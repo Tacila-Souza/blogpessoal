e29d29c3b43d9ac194f3666cdb20c0eebc340b6b
Tacila Souza <tacilasouza.contato@gmail.com>
2026-01-15 01:04:04 -0300

    step 8 do doc 13 completo

--- src/postagem/services/postagem.service.ts
+++ src/postagem/services/postagem.service.ts
@@ -11,13 +11,20 @@ export class PostagemService {
   ) {}
 
   async findAll(): Promise<Postagem[]> {
-    return await this.postagemRepository.find();
+    return await this.postagemRepository.find({
+      relations: {
+        tema: true,
+      },
+    });
   }
 
   async findById(id: number): Promise<Postagem> {
     const postagem = await this.postagemRepository.findOne({
       where: {
         id,
+      },
+      relations: {
+        tema: true,
       },
     });
 
@@ -32,10 +39,15 @@ export class PostagemService {
       where: {
         titulo: ILike(`%${titulo}%`),
       },
+      relations: {
+        tema: true,
+      },
     });
   }
 
   async creat(postagem: Postagem): Promise<Postagem> {
+    await this.findById(postagem.id);
+
     return await this.postagemRepository.save(postagem);
   }
 
--- src/postagem/tema/tema.module.ts
+++ src/postagem/tema/tema.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Tema } from './entities/tema.entity';
+import { Postagem } from '../entities/postagem.entity';
+import { TemaModule } from '../tema/tema.module';
+import { TemaController } from './controllers/tema.controller';
+import { TemaService } from './services/tema.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Tema])],
-  providers: [],
-  controllers: [],
-  exports: [],
+  imports: [TypeOrmModule.forFeature([Postagem]), TemaModule],
+  providers: [TemaService],
+  controllers: [TemaController],
+  exports: [TemaService],
 })
-export class TemaModule {}
+export class PostagemModule {}
